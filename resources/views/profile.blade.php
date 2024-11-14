5f56a4fde7172bce8d26cdaf75b8782f8f3f8f16
hex419 <kacperinos@gmail.com>
2024-11-14 00:34:52 +0000

    🐛 add auth check on admin menu profile

--- resources/views/profile.blade.php
+++ resources/views/profile.blade.php
@@ -94,7 +94,7 @@
             }
         </style>
 
-        @if(auth()->user()->is_admin)
+        @if(auth()?->user()?->is_admin)
             <div class="absolute left-4 right-4 bottom-4 bg-white rounded-full w-10 h-10 flex items-center justify-center text-center">
                 <form action="{{ route('users.toggle-banned', $user) }}" method="POST">
                     @csrf
